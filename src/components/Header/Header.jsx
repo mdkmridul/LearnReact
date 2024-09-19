97b0b25e0cf066aeec740caf3de01c74ccfe5e61
Mridul Khatri <eemridulhemani@gmail.com>
2024-09-19 10:49:59 +0530

    - component restructuring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import reactImg from '../../assets/react-core-concepts.png';
 import './Header.css';
 
-const reactDescription = ["Fundamenal", "Crucial", "Core"];
+const reactDescription = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
